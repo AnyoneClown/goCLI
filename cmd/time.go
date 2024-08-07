c51e9287c0c676fa92eaf408f4570893b28cd93b
Denis Dymerlii <dum.denya15@gmail.com>
2024-08-07 11:31:42 +0300

    add flag for timezone, now i can get time from different timezones

--- cmd/time.go
+++ cmd/time.go
@@ -6,22 +6,42 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var TimeZone string
+
 func getCurrentTime() string {
 	return time.Now().Format(time.RFC1123)
 }
 
+func getTimeInZone(timezone string) (string, error) {
+    location, err := time.LoadLocation(timezone)
+    if err != nil {
+        return "", err
+    }
+    return time.Now().In(location).Format(time.RFC1123), nil
+}
 
-var currentTime = &cobra.Command{
+
+
+var currentTimeCmd = &cobra.Command{
 	Use:   "time",
 	Short: "Gives current time",
+	Long:  `This command provides the current time. If a timezone is specified, it will return the time in that timezone.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(getCurrentTime())
+		if TimeZone != "" {
+			time, err := getTimeInZone(TimeZone)
+			if err != nil {
+				fmt.Printf("Error: %v\n", err)
+				return
+			}
+			fmt.Printf("Current time in %s: %s\n", TimeZone, time)
+		} else {
+			fmt.Printf("Current time: %s\n", getCurrentTime())
+		}
 	},
 }
 
 
 func init() {
-	rootCmd.AddCommand(currentTime)
+	rootCmd.AddCommand(currentTimeCmd)
+	currentTimeCmd.Flags().StringVarP(&TimeZone, "timezone", "z", "", "Specify a timezone (e.g., 'America/New_York')")
 }
-
-
